59983839a2b6bb8957e1160626991e319fe12c72
whoiscuzz <qwen123dmf@proton.me>
2026-01-25 20:10:54 +0300

    Initial Commit

---
+++ Chetverka/ChetverkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChetverkaApp.swift
+//  Chetverka
+//
+//  Created by whoiscuzz on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct ChetverkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
