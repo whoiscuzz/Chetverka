fec350e528922d0dfb3c0a2832d75213559897fe
whoiscuzz <qwen123dmf@proton.me>
2026-02-03 23:11:37 +0300

    clean code

--- Chetverka/Models/Profile.swift
+++ Chetverka/Models/Profile.swift
@@ -1,14 +1,13 @@
 import Foundation
 
 struct Profile: Codable, Identifiable {
-    var id: String { fullName } // Assuming fullName is unique enough for Identifiable
+    var id: String { fullName }
     let fullName: String
     let className: String?
     let avatarUrl: String?
     let classTeacher: String?
 }
 
-// MARK: - Wrapper for the entire login response
 struct LoginResponse: Codable {
     let sessionid: String
     let pupilid: String
