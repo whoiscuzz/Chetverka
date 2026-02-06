7af2f399cc7e7bab5ce7ab0b47b785e45fb92476
whoiscuzz <qwen123dmf@proton.me>
2026-02-06 22:14:04 +0300

    clean code

--- Chetverka/Views/AnalyticsViewModel.swift
+++ Chetverka/Views/AnalyticsViewModel.swift
@@ -7,7 +7,29 @@ final class AnalyticsViewModel: ObservableObject {
     @Published var totalMarks: Int = 0
     @Published var bestSubjects: [(String, Double)] = []
     @Published var weakSubjects: [(String, Double)] = []
-    @Published var pieData: [SubjectSlice] = []
+    @Published var allSubjects: [(String, Double)] = []
+    @Published var subjectGoals: [String: Double] = [:]
+
+    private let userDefaultsKey = "subjectGoals"
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: userDefaultsKey) {
+            if let decodedGoals = try? JSONDecoder().decode([String: Double].self, from: data) {
+                self.subjectGoals = decodedGoals
+            }
+        }
+    }
+
+    func saveSubjectGoal(subject: String, goal: Double?) {
+        if let goal = goal {
+            subjectGoals[subject] = goal
+        } else {
+            subjectGoals.removeValue(forKey: subject)
+        }
+        if let encoded = try? JSONEncoder().encode(subjectGoals) {
+            UserDefaults.standard.set(encoded, forKey: userDefaultsKey)
+        }
+    }
 
     func calculate(from weeks: [Week]) {
 
@@ -44,24 +66,8 @@ final class AnalyticsViewModel: ObservableObject {
             let sortedByAverage = subjectAverages.sorted { $0.1 > $1.1 }
             let best = Array(sortedByAverage.prefix(5))
             let weak = Array(subjectAverages.filter { $0.1 < 6.5 }.sorted { $0.1 < $1.1 }.prefix(5))
-
-
-            let colors: [Color] = [.blue, .green, .orange, .purple, .pink, .red, .yellow]
-
-
-            let sortedByCount = subjectMarks.sorted { $0.value.count > $1.value.count }
-
-
-            let topSubjects = sortedByCount.prefix(colors.count)
-
-
-            let slices = topSubjects.enumerated().map { index, item in
-                SubjectSlice(
-                    subject: item.key.capitalized,
-                    value: item.value.count,
-                    color: colors[index % colors.count]
-                )
-            }
+            
+            let all = subjectAverages.sorted { $0.0 < $1.0 }
 
             DispatchQueue.main.async {
                 withAnimation(.spring()) {
@@ -69,7 +75,7 @@ final class AnalyticsViewModel: ObservableObject {
                     self.totalMarks = count
                     self.bestSubjects = best
                     self.weakSubjects = weak
-                    self.pieData = slices
+                    self.allSubjects = all
                 }
             }
         }
